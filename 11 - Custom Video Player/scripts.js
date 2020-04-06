3e1fb36f5bb4b37c63e64da60026c84a2f72f57d
Mike Warren <mikeywarren89@gmail.com>
2020-04-06 13:01:00 +0900

    added progress const

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,6 +1,8 @@
  // Get Elements
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+
  // Build functions
 
  // Hook them up
