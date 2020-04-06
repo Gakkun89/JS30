a507222a00f14e46ad2f26868fc103c0737a8528
Mike Warren <mikeywarren89@gmail.com>
2020-04-06 13:03:35 +0900

    added slider cosnt

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,7 +5,7 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-
+const ranges = player.querySelectorAll('.player__slider');
  // Build functions
 
  // Hook them up
