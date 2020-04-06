1e1639d4af8f2c7333db21db3427f730484bc2c3
Mike Warren <mikeywarren89@gmail.com>
2020-04-06 13:02:48 +0900

    toggle buttons const

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,6 +4,7 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
 
  // Build functions
 
