57531b0280494f2d919d0aaea2bdd45b4dbfedea
Mike Warren <mikeywarren89@gmail.com>
2020-04-30 09:51:23 +0900

    created countdown variable

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,7 +1,13 @@
+let countdown;
+
 function timer(seconds) {
   const now = Date.now():;
   const then = now + seconds * 1000;
-  setInterval(() => {
+  countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if(secondsLeft <= 0) {
+      clearInterval(countdown);
+      return;
+    }
   }, 1000)
 }
