9b7efa2ba319c984e2954100f2b1db3bc1809733
Mike Warren <mikeywarren89@gmail.com>
2020-04-30 09:59:13 +0900

    deal with single digit seconds

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -18,6 +18,6 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-  const displayTime = `${minutes}:${remainderSeconds}`;
+  const displayTime = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
   timerDisplay.textContent = displayTime;
 }
