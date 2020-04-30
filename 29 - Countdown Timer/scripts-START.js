6833971a645cc312e3350187a6ddb6efea2e80a5
Mike Warren <mikeywarren89@gmail.com>
2020-04-30 09:57:19 +0900

    delete extra :

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -2,7 +2,7 @@ let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 
 function timer(seconds) {
-  const now = Date.now():;
+  const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
   countdown = setInterval(() => {
